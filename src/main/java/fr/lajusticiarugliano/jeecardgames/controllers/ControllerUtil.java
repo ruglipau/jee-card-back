1fdb6f1eff56f2d3c03df9b22345a6fd37e8ae3e
Evan Lajusticia <evan.lajusticia@gmail.com>
2022-01-28 16:43:31 +0100

    created ControllerUtil class for getting infos from tokens

--- src/main/java/fr/lajusticiarugliano/jeecardgames/controllers/ControllerUtil.java
+++ src/main/java/fr/lajusticiarugliano/jeecardgames/controllers/ControllerUtil.java
@@ -1,2 +1,22 @@
-package fr.lajusticiarugliano.jeecardgames.controllers;public class ControllerUtil {
+package fr.lajusticiarugliano.jeecardgames.controllers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import fr.lajusticiarugliano.jeecardgames.security.SecurityUtil;
+
+public class ControllerUtil {
+
+    public static final String getMailFromAuthorizationHeader(String header) {
+        String token = header.substring(7);
+        Algorithm algorithm = SecurityUtil.ALGORITHM;
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = verifier.verify(token);
+        String mail = decodedJWT.getSubject();
+
+        return mail;
+    }
+
+    private ControllerUtil() {}
 }
